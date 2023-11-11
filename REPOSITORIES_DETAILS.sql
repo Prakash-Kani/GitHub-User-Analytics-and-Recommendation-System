58b03811e38663375c86891f3cac5fd31c9d6600
Prakash-Kani <prakashkoffi@gmail.com>
2023-11-11 15:21:46 +0530

    Updated queries for retrieve data

--- REPOSITORIES_DETAILS.sql
+++ REPOSITORIES_DETAILS.sql
@@ -6,20 +6,20 @@ USE github;
 -- CREATING GITHUB REPOSITORIES DETAILS TABLE
  
 CREATE TABLE IF NOT EXISTS repositories_details(
-Owner VARCHAR(250) PRIMARY KEY,
+Owner VARCHAR(250),
 Repo_Name VARCHAR(250),
 Repo_Link VARCHAR(250),
 Created_At DATE,
 Description TEXT,
 Forks_Count INT,
-Repo_Fullname VARCHAR(250),
+Repo_Fullname VARCHAR(250) PRIMARY KEY,
 Repo_ID INT, 
 Language_Used VARCHAR(250),
 Last_Modified INT,
 Pushed_At DATE,
 Size INT,
 Subscriber INT,
-Sargazers INT,
+Stargazers INT,
 Updated_At DATE,
 Watchers_Counts INT, 
 Commits INT);
@@ -27,7 +27,7 @@ Commits INT);
 
 -- INSERT QUERY FOR REPOSITORIES DETAILS TABLE 
 INSERT INTO repositories_details
-(Owner, Repo_Name, Repo_Link, Created_At, Description, Forks_Count, Repo_Fullname, Repo_ID, Language_Used, Last_Modified, Pushed_At, Size, Subscriber, Sargazers, Updated_At, Watchers_Counts, Commits)
+(Owner, Repo_Name, Repo_Link, Created_At, Description, Forks_Count, Repo_Fullname, Repo_ID, Language_Used, Last_Modified, Pushed_At, Size, Subscriber, Stargazers, Updated_At, Watchers_Counts, Commits)
 VALUES ('nethajinirmal13',
  'allanasta',
  'https://github.com/nethajinirmal13/allanasta.git',
@@ -59,10 +59,24 @@ Last_Modified = VALUES(Last_Modified),
 Pushed_At = VALUES(Pushed_At), 
 Size = VALUES(Size), 
 Subscriber = VALUES(Subscriber), 
-Sargazers = VALUES(Sargazers), 
+Stargazers = VALUES(Sargazers), 
 Updated_At = VALUES(Updated_At), 
 Watchers_Counts = VALUES(Watchers_Counts), 
 Commits = VALUES(Commits);
 
+select * from repositories_details;
 
+-- To Retrieve the Repos Per Languages data.
+SELECT Language_Used as Language, COUNT(*) as Count FROM repositories_details
+                        WHERE Owner = 'Prakash-Kani' AND Language_Used IS NOT NULL
+                        GROUP BY Language_Used;
+-- To Retrieve the Commits Per Languages data.
+SELECT Language_Used as Language, sum(Commits) AS Commits FROM repositories_details
+                        WHERE Owner = 'Prakash-Kani' AND Language_Used IS NOT NULL
+                        GROUP BY Language_Used;
+                       
+-- To Retrieve the Stargazers count Per Languages data.
+SELECT Language_Used as Language, sum(Stargazers) AS Stars FROM repositories_details
+                        WHERE Owner = 'Prakash-Kani' AND Language_Used IS NOT NULL
+                        GROUP BY Language_Used;
 
