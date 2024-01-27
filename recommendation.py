a5f00ea9147e9dc971207e029296d617c97a65c1
Prakash-Kani <prakashkoffi@gmail.com>
2024-01-27 15:23:06 +0530

    Updated Recommendation User Details for Dashboard

--- recommendation.py
+++ recommendation.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import mysql.connector
 import spacy
 import base64
 from PIL import Image
@@ -10,26 +9,8 @@ import json
 import pickle
 
 from sklearn.preprocessing import OrdinalEncoder
-import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
-
-
-def get_mysql_connection():
-    mysql_host_name = os.getenv("MYSQL_HOST_NAME")
-    mysql_user_name = os.getenv("MYSQL_USER_NAME")
-    mysql_password = os.getenv("MYSQL_PASSWORD")
-    mysql_database_name = os.getenv("MYSQL_DATABASE_NAME")
-
-    mydb = mysql.connector.connect(host = mysql_host_name,
-                                user = mysql_user_name,
-                                password = mysql_password,
-                                database = mysql_database_name)
-    mycursor = mydb.cursor(buffered = True)
-
-    return mydb, mycursor
+from sklearn.decomposition import PCA
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 cluster_df = pd.read_csv(r'src/repo_clusters.csv')
@@ -56,8 +37,7 @@ file = open(r'src/Cluster_Classifying_Model.pkl', 'rb')
 knn_classifier = pickle.load(file)
 
 
-def get_repo_details():
-    mydb, mycursor = get_mysql_connection()
+def get_repo_details(mycursor):
 
     mycursor.execute("Select * from repositories_details;")
     data= mycursor.fetchall()
@@ -72,13 +52,55 @@ def get_repo_details():
 
     return repo1
 
-def Yet_to_pred(username):
+def to_get_test_data(username, mycursor):
+    query = f"""Select Repo_Name, Description, Owner, Language_Used, Commits, 
+                    Stargazers, Forks_Count, Is_Fork from repositories_details
+                    where Owner = '{username}';"""
+    mycursor.execute(query)
+    data= mycursor.fetchall()
+    user_data = pd.DataFrame(data, columns=[i[0] for i in mycursor.description])
+    user_data['Description'] = user_data['Description'].apply(lambda x:'Not Specified' if x == None  else x)
 
-    repo1 = get_repo_details()
+    repo_doc = [nlp(i) for i in user_data['Repo_Name']]
+    description_doc = [nlp(i) for i in user_data['Description']]
 
-    test_user = cluster_df[cluster_df['Owner']== unique_value['Owner']['Prakash-Kani']]
+    repo_vector = [token.vector for token in repo_doc]
+    description_vector = [token.vector for token in description_doc]
+
+    repo_df = pd.DataFrame(repo_vector)
+
+    repo_df.columns =['repo_vector_' + str(i) for i in repo_df.columns]
+
+    # Create a DataFrame with the description vectors
+    description_df = pd.DataFrame(description_vector)
+    description_df.columns = ['description_vector_' + str(i) for i in description_df.columns]
+    # Merge the two DataFrames into one
+    df1 = pd.merge(repo_df, description_df, left_index=True, right_index=True)
+
+    pca = PCA(n_components=2)
+    pca_data = pca.fit_transform(df1)
+    test_data =pd.DataFrame(pca_data, columns = ['Compnent_1', 'Compnent_2'])
+    test_data[['Owner','Language_Used', 'Commits', 'Stargazers', 'Forks_Count','Is_Fork']] = user_data[['Owner','Language_Used', 'Commits', 'Stargazers', 'Forks_Count','Is_Fork']]
+    test_data.dropna(axis = 0, inplace = True)
+
+    test_data['Language_Used'] = test_data['Language_Used'].apply(lambda x: unique_value['Language_Used'][x])
+    if username in unique_value['Owner'].keys():
+        test_data['Owner'] = test_data['Owner'].apply(lambda x: unique_value['Owner'][x])
+    else:
+        test_data['Owner'] = test_data['Owner'].apply(lambda x: len(unique_value['Owner'].keys())+1)
+
+    return test_data
+
+
+
+def Yet_to_pred(username, mycursor):
+
+    repo1 = get_repo_details(mycursor)
+    test_data = to_get_test_data(username, mycursor)
+
+    # test_user = cluster_df[cluster_df['Owner']== unique_value['Owner']['Prakash-Kani']]
     # print(test_user)
-    pred =knn_classifier.predict(test_user.drop('Predicted_Clusters', axis =1))
+    pred =knn_classifier.predict(test_data)
     pred = pd.Series(pred)
 
     user_data = repo1[repo1['Owner']== username][['Repo_Name', 'Description', 'Owner' ]]
@@ -88,7 +110,6 @@ def Yet_to_pred(username):
     cluster_details.reset_index(drop= True, inplace = True)
     user_index = cluster_details[cluster_details['Owner']==username].index
 
-
     # nlp = spacy.load('en_core_web_md')
 
     repo_doc = [nlp(i) for i in cluster_details['Repo_Name']]
@@ -96,7 +117,6 @@ def Yet_to_pred(username):
 
     repo_vector = [token.vector for token in repo_doc]
     description_vector = [token.vector for token in description_doc]
-
 
     repo_df = pd.DataFrame(repo_vector)
 
@@ -110,13 +130,26 @@ def Yet_to_pred(username):
     df1['Owner'] = cluster_details['Owner']
     return user_index, cluster_details, df1
 
-from sklearn.metrics.pairwise import cosine_similarity
-def get_similarity(username):
-    
-    user_indices, cluster_details, df = Yet_to_pred(username)
-
+def to_encoding(df3):
+    data = df3['Owner'].unique()
+    data.sort()
+    unique_value_user ={}
     enc = OrdinalEncoder()
-    df['Owner'] = enc.fit_transform(df[['Owner']])
+    df3['Owner'] = enc.fit_transform(df3[['Owner']])
+
+    globals()['Owner'] = {}
+    for i in df3['Owner'].unique():
+        globals()['Owner'][data[int(i)]] = i
+        unique_value_user['Owner'] = globals()['Owner']
+    return df3, unique_value_user
+
+
+def get_similarity(username, mycursor):
+    
+    user_indices, cluster_details, df = Yet_to_pred(username, mycursor)
+
+    df, unique_value_user = to_encoding(df[['Owner']])
+    # df['Owner'] = enc.fit_transform(df[['Owner']])
 
     similarity_matrix = cosine_similarity(df)
 
@@ -127,9 +160,9 @@ def get_similarity(username):
 
     # Sort the DataFrame by similarity scores in descending order
     sorted_similarity_df = similarity_df.sort_values(by='Similarity', ascending=False)
-
+   
     # Exclude the user's own indices
-    sorted_similarity_df = sorted_similarity_df[sorted_similarity_df['Owner']!= unique_value['Owner'][username]]
+    sorted_similarity_df = sorted_similarity_df[sorted_similarity_df['Owner']!= unique_value_user['Owner'][username]]
 
     top_10_recommendations = sorted_similarity_df.head(10)
 
@@ -138,11 +171,9 @@ def get_similarity(username):
     return [unique_value_ref[owner] for owner in sorted_similarity_df.Owner.unique()[:10]]
 
 
-def top10_user_recommedation(username):
+def top10_user_recommedation(username, mycursor):
 
-    similarity_user = get_similarity(username)
-
-    mydb, mycursor = get_mysql_connection()
+    similarity_user = get_similarity(username, mycursor)
 
     query = f"""SELECT User_Name, Login, Public_Repos, Avatar_url, Profile_url FROM user_details 
                 WHERE Login IN {tuple(similarity_user)};"""
@@ -150,7 +181,30 @@ def top10_user_recommedation(username):
     mycursor.execute(query)
     data= mycursor.fetchall()
     recommend = pd.DataFrame(data, columns=[i[0] for i in mycursor.description])
-    print(recommend)
-
+    
     return recommend
 
+
+def recommendation_details(df):
+
+    userdetails = []
+    for i in range(9):
+        url = df['Avatar_url'].iloc[i]
+        response = requests.get(url)
+        img = Image.open(BytesIO(response.content))
+        img_bytes = BytesIO()
+        img.save(img_bytes, format='PNG')
+        encoded_img = 'data:image/png;base64,' + base64.b64encode(img_bytes.getvalue()).decode()
+        image = html.Img(src=encoded_img, style={'width': '90%', 'height': '80%'})
+        userdetails.append(image)
+
+    for i in range(9):
+        profile = [dcc.Markdown(f'#### ***Top {i+1} User Details***', style = {'color': '#4090F5'}),
+                  dcc.Markdown(f''' > User Name : {df.User_Name.iloc[i]}
+                                \n > Login Name :{df.Login.iloc[i]}
+                                \n > Repo Count : {df.Public_Repos.iloc[i]}
+                                \n > [View Profile on Github]({df.Profile_url.iloc[i]})''', style = {'color': '#4090F5',})]
+               
+        userdetails.append(profile)
+
+    return userdetails
