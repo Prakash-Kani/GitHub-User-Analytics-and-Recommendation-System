5d0b24af68c2ee4474d3aec1fef770b1d949a24e
Prakash-Kani <prakashkoffi@gmail.com>
2024-01-27 15:24:41 +0530

    Updated Recommendation tab's Layout Content

--- render_file.py
+++ render_file.py
@@ -1,5 +1,11 @@
 from dash import Dash, html, dcc
 
+def User_Details(mycursor):
+    mycursor.execute('SELECT DISTINCT(Login) FROM user_details;')
+    data = mycursor.fetchall()
+    uniquelogin = [i[0] for i in data]
+    return uniquelogin
+
 def Extracting_Data_tab():
     page =[
         html.Div([
@@ -17,7 +23,8 @@ def Extracting_Data_tab():
 
 
 
-def Analysis_tab(uniquelogin):
+def Analysis_tab(mycursor):
+    uniquelogin = User_Details(mycursor)
     page = [html.Div([
             html.Div([
 
@@ -52,6 +59,52 @@ def Analysis_tab(uniquelogin):
                 html.Div(dcc.Graph(id='Stars Per Repo fig'), style={'width': '50%', 'display': 'inline-block', 'backgroundColor':"#E8CFCA"}),
                 html.Hr(),
             ], style = {'display': 'flex', 'flex-direction': 'row'})
+    ])
+
+            ]
+
+    return page
+
+def Recommendation_tab(mycursor):
+    uniquelogin = User_Details(mycursor)
+    page = [html.Div([
+            html.Div([
+               html.Label('Select the Username'),
+                dcc.Dropdown(
+                id = "user-login-name1",
+                options = [{"label": x, "value": x} for x in uniquelogin],
+                value = uniquelogin[0],
+                clearable = False,
+            ),
+            ]),
+
+    ]),
+    html.Hr(),
+        html.Div([
+            html.Div([html.Div(id = 'user1', style={'width': '13%', 'display': 'inline-block'}), 
+                      html.Div(id = 'user1_details', style={'width': '30%', 'display': 'inline-block'}),
+                      html.Div(id = 'user2', style={'width': '13%', 'display': 'inline-block'}), 
+                      html.Div(id = 'user2_details', style={'width': '30%', 'display': 'inline-block'}),
+                      html.Div(id = 'user3', style={'width': '14%', 'display': 'inline-block'}), 
+                      html.Div(id = 'user3_details', style={'width': '30%', 'display': 'inline-block'})], style={'display': 'flex', 'flex-direction': 'row'}),
+
+            html.Hr(),
+            html.Div([html.Div(id = 'user4', style={'width': '13%', 'display': 'inline-block'}), 
+                      html.Div(id = 'user4_details', style={'width': '30%', 'display': 'inline-block'}),
+                      html.Div(id = 'user5', style={'width': '13%', 'display': 'inline-block'}), 
+                      html.Div(id = 'user5_details', style={'width': '30%', 'display': 'inline-block'}),
+                      html.Div(id = 'user6', style={'width': '14%', 'display': 'inline-block'}), 
+                      html.Div(id = 'user6_details', style={'width': '30%', 'display': 'inline-block'})], style = {'display': 'flex', 'flex-direction': 'row'}),
+            
+            html.Hr(),
+            html.Div([html.Div(id = 'user7', style={'width': '13%', 'display': 'inline-block'}), 
+                      html.Div(id = 'user7_details', style={'width': '30%', 'display': 'inline-block'}),
+                      html.Div(id = 'user8', style={'width': '13%', 'display': 'inline-block'}), 
+                      html.Div(id = 'user8_details', style={'width': '30%', 'display': 'inline-block'}),
+                      html.Div(id = 'user9', style={'width': '14%', 'display': 'inline-block'}), 
+                      html.Div(id = 'user9_details', style={'width': '30%', 'display': 'inline-block'})], style = {'display': 'flex', 'flex-direction': 'row'}),
+
+        ], style = {'display': 'flex', 'flex-direction': 'column'})
 
             ]
 
